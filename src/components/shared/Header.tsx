e42705f97aab4b614290ec73501c54ac89e67edd
irishabhnegi <rishabhnegi175@gmail.com>
2024-01-10 17:48:15 +0530

    header hide on scroll

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -5,22 +5,72 @@ import cart from '../../assets/cart.svg';
 import profile from '../../assets/profile.svg';
 import save from '../../assets/save.svg';
 import search from '../../assets/search.svg';
+import cartLight from '../../assets/cart-light.svg';
+import profileLight from '../../assets/profile-light.svg';
+import saveLight from '../../assets/save-light.svg';
+import searchLight from '../../assets/search-light.svg';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
   const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  const show = 'bg-gray-700 transition-all duration-500';
+  const top = 'translate-y-0 transition-all duration-500 bg-white';
+  const hide = '-translate-y-16 transition-all duration-500';
+
+  const [navStatus, setNavStatus] = useState(top);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [showStatusColor, setShowStatusColor] = useState(false);
+
+  const controlNavbar = () => {
+    const currentScrollY = window.scrollY;
+    if (currentScrollY > 200) {
+      if (currentScrollY > lastScrollY) {
+        setNavStatus(hide);
+        setShowStatusColor(false);
+      } else if (currentScrollY < lastScrollY - 2) {
+        setNavStatus(show);
+        setShowStatusColor(true);
+      }
+    } else {
+      setNavStatus(top);
+      setShowStatusColor(false);
+    }
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+    return () => window.removeEventListener('scroll', controlNavbar);
+  }, [lastScrollY]);
+
+  const handleSearchBar = () => {
+    console.log('search');
+  };
+
   return (
-    <div className='w-full px-10 flex justify-between fixed '>
-      <ul className='w-[25rem]   flex gap-5 items-center justify-start'>
+    <header
+      className={`w-full h-16 px-10 flex justify-between fixed ${navStatus} `}>
+      <ul className='w-[25rem]  h-auto flex gap-5 items-center justify-start'>
         {headerNavList.map((item) => {
           const isActive = pathname === item.route ? true : false;
           return (
             <li key={item.label}>
               <Link
                 to={item.route}
-                className={`text-sm font-medium ${
-                  isActive && 'text-green-700'
-                } `}>
+                className={`text-sm  ${
+                  isActive
+                    ? 'text-green-500 font-bold'
+                    : 'text-gray-900 font-medium'
+                } ${
+                  showStatusColor && !isActive
+                    ? 'text-gray-100 font-medium'
+                    : ''
+                }`}>
                 {item.label}
               </Link>
             </li>
@@ -28,7 +78,7 @@ const Header = () => {
         })}
       </ul>
 
-      <div className='w-[25rem]  flex justify-center items-center'>
+      <div className='w-[25rem] h-auto  flex justify-center items-center'>
         <Link to='/'>
           <img
             src={logo}
@@ -38,33 +88,43 @@ const Header = () => {
         </Link>
       </div>
 
-      <ul className=' w-[25rem] flex items-center justify-end gap-5'>
+      <ul className=' w-[25rem] h-auto flex items-center justify-end gap-5 '>
         <li>
-          <img
-            src={search}
-            alt='seaarch'
-          />
+          <button
+            type='button'
+            onClick={handleSearchBar}>
+            <img
+              src={showStatusColor ? searchLight : search}
+              alt='search'
+            />
+          </button>
         </li>
         <li>
-          <img
-            src={profile}
-            alt='profile'
-          />
+          <button>
+            <img
+              src={showStatusColor ? profileLight : profile}
+              alt='profile'
+            />
+          </button>
         </li>
         <li>
-          <img
-            src={save}
-            alt='save'
-          />
+          <button>
+            <img
+              src={showStatusColor ? saveLight : save}
+              alt='save'
+            />
+          </button>
         </li>
         <li>
-          <img
-            src={cart}
-            alt='cart'
-          />
+          <button>
+            <img
+              src={showStatusColor ? cartLight : cart}
+              alt='cart'
+            />
+          </button>
         </li>
       </ul>
-    </div>
+    </header>
   );
 };
 export default Header;
